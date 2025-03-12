531de69b166ebb08dc08444975c0a20d4702b0d4
22406972 <naufal.somantri@ens.uvsq.fr>
2025-03-12 07:54:51 +0100

    Tentatif HTTPS 2

--- back/src/app.js
+++ back/src/app.js
@@ -54,14 +54,12 @@ app.get("/", (req, res) => {
   res.send("Le backend fonctionne !");
 });
 
-/*
 app.use(cors({
   origin: allowedOrigins,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-*/
 
 // Gestion des sessions
 /*
