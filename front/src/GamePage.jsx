427eb8c7b50fa37dc416897dca95b4f9da187e43
uvsq22106020 <amira.hamad@ens.uvsq.fr>
2025-03-21 03:30:05 +0100

    ajout compte a rebours

--- front/src/GamePage.jsx
+++ front/src/GamePage.jsx
@@ -2,89 +2,72 @@ import React, { useState, useEffect } from 'react';
 import "./GamePage.css";
 
 function removeAccents(str) {
-  // Normalise la chaîne et enlève les diacritiques
-  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  return str.normalize("NFD").replace(/[̀-ͯ]/g, "");
 }
 
-const GamePage = ({ setCurrentPage, initialLives, initialTime, livesLostThreshold, keyboardColor}) => {
+const GamePage = ({ setCurrentPage, initialLives, initialTime, livesLostThreshold, keyboardColor }) => {
   const [sequence, setSequence] = useState("");
   const [inputValue, setInputValue] = useState("");
   const [lives, setLives] = useState(initialLives);
-  const [socket, setSocket] = useState(null); // WebSocket state
-  const [gameOver, setGameOver] = useState(false); // Game over state
+  const [socket, setSocket] = useState(null);// WebSocket state
+  const [gameOver, setGameOver] = useState(false);// Game over state
   const [timer, setTimer] = useState(initialTime);
   const [timerInterval, setTimerInterval] = useState(null); // Intervalle pour le timer
   const [lostLivesCount, setLostLivesCount] = useState(0);
   const [currentKeyboardColor, setCurrentKeyboardColor] = useState(localStorage.getItem("keyboardColor") || keyboardColor);
+  const [countdown, setCountdown] = useState(3);
+  const [gameStarted, setGameStarted] = useState(false);
 
   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
   const [blackenedLetters, setBlackenedLetters] = useState(new Set());
 
-
   useEffect(() => {
-        const savedColor = localStorage.getItem("keyboardColor");
-        if (savedColor) {
-            setCurrentKeyboardColor(savedColor);
-        }
-    }, []);
-
-  // Connecter au WebSocket backend
+    const savedColor = localStorage.getItem("keyboardColor");
+    if (savedColor) {
+      setCurrentKeyboardColor(savedColor);
+    }
+  }, []);
+  
+    // Connecter au WebSocket backend
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:4002");
     setSocket(ws);
-    
-    ws.onopen = () => {
-      console.log("WebSocket connecté !");
-    };
-    
-    ws.onerror = (error) => {
-      console.log("Erreur WebSocket :", error);
-    };
-    
-    ws.onclose = (event) => {
-      console.log("Connexion WebSocket fermée", event);
-    };
-    
+
+    ws.onopen = () => console.log("WebSocket connecté !");
+    ws.onerror = (error) => console.log("Erreur WebSocket :", error);
+    ws.onclose = (event) => console.log("Connexion WebSocket fermée", event);
     ws.onmessage = (event) => {
       const data = JSON.parse(event.data);
-      console.log("Message reçu du serveur :", data);
-
-      if (data.type === 'game_over') {
-        setGameOver(true);
-        alert("Le jeu est terminé!");
-      }
-
-      if (data.type === 'update_lives') {
-        setLives(data.lives);
-      }
+      if (data.type === 'game_over') setGameOver(true);
+      if (data.type === 'update_lives') setLives(data.lives);
     };
-
-    return () => {
-      ws.close(); // Fermer la connexion lors de la fermeture du composant
-    };
+    return () => ws.close();
   }, []);
+
+  useEffect(() => {
+    if (countdown > 0) {
+      const countdownInterval = setInterval(() => setCountdown(prev => prev - 1), 1000);
+      return () => clearInterval(countdownInterval);
+    } else {
+      setGameStarted(true);
+    }
+  }, [countdown]);
 
   function loseLife() {
     // Réduire le nombre de vies
-    setLives((prevLives) => {
+    setLives(prevLives => {
       const newLives = prevLives - 1;
-  
-      // Gérer la fin de partie
+
+       // Gérer la fin de partie
       if (newLives <= 0) {
         setGameOver(true);
         socket.send(JSON.stringify({ type: 'game_over' }));
-        alert("Fin de la partie !");
       } else {
         // Mettre à jour le serveur
-        socket.send(JSON.stringify({
-          type: 'update_lives',
-          lives: newLives,
-        }));
+        socket.send(JSON.stringify({ type: 'update_lives', lives: newLives }));
       }
-  
       return newLives;
     });
-  
     // Incrémenter le compteur de vies perdues
     setLostLivesCount((prevCount) => {
       const updatedCount = prevCount + 1;
@@ -99,8 +82,7 @@ const GamePage = ({ setCurrentPage, initialLives, initialTime, livesLostThreshol
       return updatedCount;
     });
   }
-
-  // Fonction pour récupérer une séquence depuis l'API
+   // Fonction pour récupérer une séquence depuis l'API
   const generateSequence = async () => {
     try {
       const response = await fetch("http://localhost:4001/random-sequence");
@@ -208,57 +190,46 @@ const GamePage = ({ setCurrentPage, initialLives, initialTime, livesLostThreshol
 
   return (
     <div className="game-container">
-      <h1>BombParty</h1>
-      <div className="lives">{Array(lives).fill("❤️").join(" ")}</div>
-      <div className="sequence">{sequence}</div>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="Entrez un mot contenant la séquence..."
-        disabled={gameOver}
-      />
-      <button onClick={handleSubmit} disabled={gameOver || !inputValue}>
-        Valider
-      </button>
-      <div 
-        className="keyboard" 
-          >
-      {letters.map((letter, index) => {
-        // Vérifier si la lettre est noircies
-        const isBlackened = blackenedLetters.has(letter);
-      
-        return (
-          <div
-            key={index}
-            className={`key ${isBlackened ? 'blackened' : ''}`}
-            style={{
-              backgroundColor: isBlackened ? "gray" : currentKeyboardColor, // Chaque touche a sa couleur
-              color: isBlackened ? "white" : "black", // Texte blanc si noircie
-              padding: '10px',
-              borderRadius: '5px',
-              margin: '5px',
-              display: 'inline-block',
-              width: '40px',
-              textAlign: 'center', 
-              cursor: isBlackened ? "not-allowed" : "pointer"
-            }}
-          >
-            {letter}
+      {countdown > 0 ? (
+        <div className="countdown">Début dans {countdown}...</div>
+      ) : (
+        <>
+          <h1>BombParty</h1>
+          <div className="lives">{Array(lives).fill("❤️").join(" ")}</div>
+          <div className="sequence">{sequence}</div>
+          <input type="text" value={inputValue} onChange={handleInputChange} placeholder="Entrez un mot contenant la séquence..." disabled={gameOver} />
+          <button onClick={handleSubmit} disabled={gameOver || !inputValue}>Valider</button>
+          <div className="bomb-container">
+            <img src="/images/bomb-icon.png" alt="Bombe" className="bomb-icon" />
+            <div className="timer">{timer}</div>
           </div>
-        );
-      })}
-      </div>
-
-      <button onClick={handleReturn}>Retour</button>
-      {gameOver && <div>Jeu terminé !</div>}
-
-      {/* Afficher la bombe avec le timer */}
-      <div className="bomb-container">
-        <img src="/images/bomb-icon.png" alt="Bombe" className="bomb-icon" />
-        <div className="timer">{timer}</div>
-      </div>
+          <div className="keyboard">
+            {letters.map((letter, index) => (
+              <div
+                key={index}
+                className={`key ${blackenedLetters.has(letter) ? 'blackened' : ''}`}
+                style={{
+                  backgroundColor: blackenedLetters.has(letter) ? "gray" : currentKeyboardColor,
+                  color: blackenedLetters.has(letter) ? "white" : "black",
+                  padding: '10px',
+                  borderRadius: '5px',
+                  margin: '5px',
+                  display: 'inline-block',
+                  width: '40px',
+                  textAlign: 'center',
+                  cursor: blackenedLetters.has(letter) ? "not-allowed" : "pointer"
+                }}
+              >
+                {letter}
+              </div>              
+            ))}
+          </div>
+          <button onClick={handleReturn}>Retour</button>
+          {gameOver && <div>Jeu terminé !</div>}
+        </>
+      )}
     </div>
+    
   );
 };
 
