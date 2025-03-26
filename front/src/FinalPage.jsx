32a4923acf99b253963296c96010bcbef11ef82a
22406972 <naufal.somantri@ens.uvsq.fr>
2025-03-26 05:53:49 +0100

    Projet Final

--- front/src/FinalPage.jsx
+++ front/src/FinalPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const FinalPage = ({ setCurrentPage }) => {
   let winner = JSON.parse(localStorage.getItem('winner'));
